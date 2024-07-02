63e870b27138cf94d0a0e5dc3c7878d64918b057
mansijmaharzn <mansijmaharzn@gmail.com>
2024-07-02 15:52:59 +0545

    change to JWT Token system

--- team_mgmt_sys/users/urls.py
+++ team_mgmt_sys/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from .views import RegisterAPI, LoginAPI, LogoutAPI
 
@@ -6,4 +7,7 @@ urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/', LogoutAPI.as_view(), name='logout'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- team_mgmt_sys/users/views.py
+++ team_mgmt_sys/users/views.py
@@ -1,39 +1,43 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from users.serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 
 class RegisterAPI(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            login(request, user)
+            refresh = RefreshToken.for_user(user)
 
             return Response({
-                "user": UserSerializer(user).data
+                "user": UserSerializer(user).data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
             }, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 class LoginAPI(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         serializer = LoginSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data
-            login(request, user)
+            refresh = RefreshToken.for_user(user)
             return Response({
                 "user": UserSerializer(user).data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
             }, status=status.HTTP_200_OK)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 class LogoutAPI(APIView):
-    def post(self, request, *args, **kwargs):
-        logout(request)
+    def post(self, request):
         return Response(status=status.HTTP_200_OK)
