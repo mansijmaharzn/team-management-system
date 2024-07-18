883cad5506a4d9ae5329cabfa42fd2b37185ba2a
mansijmaharzn <mansijmaharzn@gmail.com>
2024-07-18 15:48:39 +0545

    update urls

--- team_mgmt_sys/teams/urls.py
+++ team_mgmt_sys/teams/urls.py
@@ -15,19 +15,23 @@ from teams.views import (
 
 
 urlpatterns = [
+    # Team-related endpoints
     path("create/", TeamCreateAPIView.as_view(), name="team-create"),
-    path("my-teams/", TeamListAPIView.as_view(), name="team-show"),
-    path("<int:pk>/team-detail/", TeamDetailAPIView.as_view(), name="team-detail"),
-    path("<int:pk>/add-member/", AddMemberAPIView.as_view(), name="add_member"),
+    path("my-teams/", TeamListAPIView.as_view(), name="team-list"),
+    path("<int:pk>/", TeamDetailAPIView.as_view(), name="team-detail"),
+    path("<int:pk>/add-member/", AddMemberAPIView.as_view(), name="team-add-member"),
     path(
-        "<int:pk>/remove-member/", RemoveMemberAPIView.as_view(), name="remove_member"
+        "<int:pk>/remove-member/",
+        RemoveMemberAPIView.as_view(),
+        name="team-remove-member",
     ),
-    path("create-tasks/", TaskCreateAPIView.as_view(), name="task-create"),
-    path("my-tasks/", TaskListAPIView.as_view(), name="task-show"),
+    # Task-related endpoints
+    path("tasks/create/", TaskCreateAPIView.as_view(), name="task-create"),
+    path("tasks/my-tasks/", TaskListAPIView.as_view(), name="task-list"),
     path(
-        "my-tasks/<int:pk>/update/",
+        "tasks/<int:pk>/update-status/",
         TaskStatusUpdateAPIView.as_view(),
-        name="task-update",
+        name="task-update-status",
     ),
     path("tasks/<int:pk>/details/", TeamTaskStatusView.as_view(), name="task-details"),
     path(
